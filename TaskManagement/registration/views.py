ef32cfff11609ae06221fa6990585d0d1928d73c
akshitjoshi9@gmail.com <akshitjoshi9@gmail.com>
2021-07-28 11:48:22 +0530

    [ADD]LoginMixin

--- TaskManagement/registration/views.py
+++ TaskManagement/registration/views.py
@@ -1,11 +1,20 @@
 from django.views.generic import TemplateView
+from django.contrib.auth.decorators import login_required
+from django.utils.decorators import method_decorator
 
 # Create your views here.
+
+
+class LoginRequiredMixin(object):
+    @method_decorator(login_required)
+    def dispatch(self, request, *args, **kwargs):
+        return super(LoginRequiredMixin, self).dispatch(request, *args,
+                                                        **kwargs)
 
 
 class Home(TemplateView):
     template_name = "tests/index.html"
 
 
-class Dashboard(TemplateView):
+class Dashboard(LoginRequiredMixin, TemplateView):
     template_name = "tests/dashboard.html"
