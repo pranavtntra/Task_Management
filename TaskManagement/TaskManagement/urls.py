70f6a0207b6745eb9af25df3c1687da221413cc0
palakparmarr <parmarpalak2000@gmail.com>
2021-08-18 12:40:17 +0530

    [RESOLVED] conflict file

--- TaskManagement/TaskManagement/urls.py
+++ TaskManagement/TaskManagement/urls.py
@@ -18,13 +18,6 @@ from django.urls import path, include
 from django.views.generic import TemplateView
 
 urlpatterns = [
-<<<<<<< HEAD
-    path('admin/', admin.site.urls),
-    path('accounts/', include('allauth.urls')),
-    path('dashboard/', TemplateView.as_view(template_name='dashboard.html'), name="dashboard"),
-    path('', TemplateView.as_view(template_name="index.html")),
-    path('djrichtextfield/', include('djrichtextfield.urls')),
-=======
     path("admin/", admin.site.urls),
     path("accounts/", include("allauth.urls")),
     path(
@@ -35,6 +28,5 @@ urlpatterns = [
     path("", TemplateView.as_view(template_name="index.html")),
     path("project/", include("project.urls")),
     path("djrichtextfield/", include("djrichtextfield.urls")),
->>>>>>> cb0a3896fbfee675a93348d464b8f8bcbee83dfd
     path('task/', include('task.urls')),
 ]
