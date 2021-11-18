b1b9ca6f03e9b35d9f2f2c4b1a4b79f1693346da
PalakParmarr <parmarpalak2000@gmail.com>
2021-11-18 18:42:45 +0530

    [UPDATED] indetations

--- TaskManagement/accounts/forms.py
+++ TaskManagement/accounts/forms.py
@@ -135,22 +135,8 @@ class UserUpdateForm(forms.ModelForm):
 
 
 class PasswordChangeForm(PasswordChangeForm):
-        error_css_class = 'has-error'
-        error_messages = {'password_incorrect':
-                  "The old password is not correct. Please try again."}
-        old_password = forms.CharField(required=True, label='Old Password',
-                      widget=forms.PasswordInput(attrs={
-                        'class': 'form-control'}),
-                      error_messages={
-                        'required': 'The password can not be empty'})
-
-        new_password1 = forms.CharField(required=True, label='New Password',
-                      widget=forms.PasswordInput(attrs={
-                        'class': 'form-control'}),
-                      error_messages={
-                        'required': 'The password can not be empty'})
-        new_password2 = forms.CharField(required=True, label='New Password (Repeat)',
-                      widget=forms.PasswordInput(attrs={
-                        'class': 'form-control'}),
-                      error_messages={
-                        'required': 'The password can not be empty'})
+    error_css_class = 'has-error'
+    error_messages = {'password_incorrect': "The old password is not correct. Please try again."}
+    old_password = forms.CharField(required=True, label='Old Password', widget=forms.PasswordInput(attrs={'class': 'form-control'}), error_messages={'required': 'The password can not be empty'})
+    new_password1 = forms.CharField(required=True, label='New Password', widget=forms.PasswordInput(attrs={'class': 'form-control'}), error_messages={'required': 'The password can not be empty'})
+    new_password2 = forms.CharField(required=True, label='New Password (Repeat)', widget=forms.PasswordInput(attrs={'class': 'form-control'}), error_messages={'required': 'The password can not be empty'})
