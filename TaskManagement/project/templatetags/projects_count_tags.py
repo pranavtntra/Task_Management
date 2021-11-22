6aacd43e595b42b9005b490c0e0257cb87fbf2d9
PalakParmarr <parmarpalak2000@gmail.com>
2021-11-22 18:17:43 +0530

    [ADD] template tags for project allocation count and list of project

--- TaskManagement/project/templatetags/projects_count_tags.py
+++ TaskManagement/project/templatetags/projects_count_tags.py
@@ -30,5 +30,21 @@ def get_role_count(proj_id):
 
 @register.simple_tag
 def get_project_count(user_id):
+    """ for count of project allocation to project manager and employee. """
     task = len(set([task.project for task in Task.objects.filter(assigned_to=user_id)]))
     return Project.objects.filter(project_lead=user_id).count() or task
+
+@register.simple_tag
+def get_project_title(user_id):
+    """ for display a list of projects which are allocated to project manager and employee
+        tasks : to get project list of employee,
+        projects : to get project list of employee,
+        we used update method to join both list and for get title
+    """
+    tasks = set([task.project for task in Task.objects.filter(assigned_to=user_id)])
+    projects = Project.objects.filter(project_lead=user_id)
+    tasks.update(set(projects))
+    data = ''
+    for p_title in tasks:
+        data += p_title.title +","
+    return data[:-1]
