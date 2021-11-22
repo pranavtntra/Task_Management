ec6aba3ee423d390e76cbf467fd43edfa9767130
PalakParmarr <parmarpalak2000@gmail.com>
2021-11-22 12:33:10 +0530

    [UPDATED] indetations

--- TaskManagement/accounts/forms.py
+++ TaskManagement/accounts/forms.py
@@ -61,37 +61,43 @@ class AddUserForm(forms.ModelForm):
         required=True,
         max_length=122,
         widget=forms.TextInput(
-            attrs={"class": "form-control", "placeholder": "Enter your email name"}
+            attrs={"class": "form-control",
+                   "placeholder": "Enter your email name"}
         ),
     )
     username = forms.CharField(
         max_length=122,
         widget=forms.TextInput(
-            attrs={"class": "form-control", "placeholder": "Enter your username"}
+            attrs={"class": "form-control",
+                   "placeholder": "Enter your username"}
         ),
     )
     first_name = forms.CharField(
         max_length=122,
         widget=forms.TextInput(
-            attrs={"class": "form-control", "placeholder": "Enter your first name"}
+            attrs={"class": "form-control",
+                   "placeholder": "Enter your first name"}
         ),
     )
     last_name = forms.CharField(
         max_length=122,
         widget=forms.TextInput(
-            attrs={"class": "form-control", "placeholder": "Enter your last name"}
+            attrs={"class": "form-control",
+                   "placeholder": "Enter your last name"}
         ),
     )
     contact = PhoneNumberField(
         widget=forms.TextInput(
-            attrs={"class": "form-control", "placeholder": "Enter your contact number"}
+            attrs={"class": "form-control",
+                   "placeholder": "Enter your contact number"}
         )
     )
     designation = forms.ChoiceField(choices=Designation)
 
     class Meta:
         model = User
-        fields = ('email', 'username', 'first_name', 'last_name', 'contact', 'designation')
+        fields = ('email', 'username', 'first_name',
+                  'last_name', 'contact', 'designation')
 
 
 class UserUpdateForm(forms.ModelForm):
@@ -101,31 +107,36 @@ class UserUpdateForm(forms.ModelForm):
     username = forms.CharField(
         max_length=122,
         widget=forms.TextInput(
-            attrs={"class": "form-control", "placeholder": "Enter your username"}
+            attrs={"class": "form-control",
+                   "placeholder": "Enter your username"}
         ),
     )
     first_name = forms.CharField(
         max_length=122,
         widget=forms.TextInput(
-            attrs={"class": "form-control", "placeholder": "Enter your first name"}
+            attrs={"class": "form-control",
+                   "placeholder": "Enter your first name"}
         ),
     )
     last_name = forms.CharField(
         max_length=122,
         widget=forms.TextInput(
-            attrs={"class": "form-control", "placeholder": "Enter your last name"}
+            attrs={"class": "form-control",
+                   "placeholder": "Enter your last name"}
         ),
     )
     contact = PhoneNumberField(
         widget=forms.TextInput(
-            attrs={"class": "form-control", "placeholder": "Enter your contact number"}
+            attrs={"class": "form-control",
+                   "placeholder": "Enter your contact number"}
         )
     )
     email = forms.EmailField(
         required=True,
         max_length=122,
         widget=forms.TextInput(
-            attrs={"class": "form-control", "placeholder": "Enter your email name"}
+            attrs={"class": "form-control",
+                   "placeholder": "Enter your email name"}
         ),
     )
 
@@ -136,7 +147,11 @@ class UserUpdateForm(forms.ModelForm):
 
 class PasswordChangeForm(PasswordChangeForm):
     error_css_class = 'has-error'
-    error_messages = {'password_incorrect': "The old password is not correct. Please try again."}
-    old_password = forms.CharField(required=True, label='Old Password', widget=forms.PasswordInput(attrs={'class': 'form-control'}), error_messages={'required': 'The password can not be empty'})
-    new_password1 = forms.CharField(required=True, label='New Password', widget=forms.PasswordInput(attrs={'class': 'form-control'}), error_messages={'required': 'The password can not be empty'})
-    new_password2 = forms.CharField(required=True, label='New Password (Repeat)', widget=forms.PasswordInput(attrs={'class': 'form-control'}), error_messages={'required': 'The password can not be empty'})
+    error_messages = {
+        'password_incorrect': "The old password is not correct. Please try again."}
+    old_password = forms.CharField(required=True, label='Old Password', widget=forms.PasswordInput(
+        attrs={'class': 'form-control'}), error_messages={'required': 'The password can not be empty'})
+    new_password1 = forms.CharField(required=True, label='New Password', widget=forms.PasswordInput(
+        attrs={'class': 'form-control'}), error_messages={'required': 'The password can not be empty'})
+    new_password2 = forms.CharField(required=True, label='New Password (Repeat)', widget=forms.PasswordInput(
+        attrs={'class': 'form-control'}), error_messages={'required': 'The password can not be empty'})
