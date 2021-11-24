2be2498150cd2737d99b0561f301f9109961ab01
PalakParmarr <parmarpalak2000@gmail.com>
2021-11-24 10:21:32 +0530

    [ADD] register technology and intermediateusertech model

--- TaskManagement/accounts/admin.py
+++ TaskManagement/accounts/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
-from accounts.models import User
+from accounts.models import User, Technology, IntermediateUserTech
 
 # Register your models here.
 
 admin.site.register(User)
+admin.site.register(Technology)
+admin.site.register(IntermediateUserTech)
