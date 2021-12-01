dc4ee71651a9e61cdeab176c2f09d3d2b9d7c78e
PalakParmarr <parmarpalak2000@gmail.com>
2021-12-01 14:44:36 +0530

    [MODIFIED] reduce code of topic js

--- TaskManagement/static/js/jquery/topic.js
+++ TaskManagement/static/js/jquery/topic.js
@@ -2,10 +2,5 @@ $('.technologies > li').each( function() {
     $(this).after('<input type="text" name="topic" required id="id_topic" disabled="disabled" >');
 });
 $('input[type="checkbox"]').click( function() {
-    if($(this).is(':checked')){
-        $(this).parent().parent().next().removeAttr("disabled");
-    }
-    else{
-        $(this).parent().parent().next().attr("disabled","disabled");
-    }
+    $(this).closest('li').next('input').attr("disabled", !$(this).is(':checked'));
 });
